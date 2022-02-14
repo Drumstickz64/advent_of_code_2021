9386f6cd2258ae2dfd1520a4691880f500bd5881
lunkk <gearman533@gmail.com>
2022-02-15 02:12:08 +0300

    finish day 2 part 1

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 mod day1;
+mod day2;
 
 fn main() {
     println!("Day 1 part 1: {}", day1::part_one());
     println!("Day 1 part 2: {}", day1::part_two());
+
+    println!("Day 2 part 1: {}", day2::part_one());
 }
